e578609f42a11f65d830b6c96883fa797b767e7d
Kiran Anil <anil007kiran@gmail.com>
2018-03-13 16:10:32 +0530

    Update task.sh

--- tasks/export-om-installation/task.sh
+++ tasks/export-om-installation/task.sh
@@ -2,4 +2,4 @@
 
 . $(dirname $0)/../../scripts/om-cmd
 
-om_cmd --request-timeout 7200 export-installation --output-file  $(dirname $0)/om-installation/installation.zip
+om_cmd --request-timeout 7200 export-installation --output-file  $(dirname $0)/../../om-installation/installation.zip
