412c75eae8b27188f1eac3fabd0586127e3c0b22
Kiran Anil <anil007kiran@gmail.com>
2018-03-14 15:28:06 +0530

    Update task.sh

--- tasks/bbr-backup-ert/task.sh
+++ tasks/bbr-backup-ert/task.sh
@@ -7,7 +7,7 @@ pushd ../../../ert-backup-artifact
   --username "${BOSH_CLIENT}" \
   --deployment "${ERT_DEPLOYMENT_NAME}" \
   --ca-cert "${BOSH_CA_CERT_PATH}" \
-  backup-cleanup 
+  backup --with-manifest
 
   tar -cvf ert-backup.tar -- *
 popd
