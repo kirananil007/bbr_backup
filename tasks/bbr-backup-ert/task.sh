06899f2dad510823964d260d2b75aaeb1da2ec0d
Kiran Anil <anil007kiran@gmail.com>
2018-03-14 16:48:18 +0530

    Update task.sh

--- tasks/bbr-backup-ert/task.sh
+++ tasks/bbr-backup-ert/task.sh
@@ -4,6 +4,10 @@
 
 bakup_time=$(date +"%F"-"%S")
 
+cd ../../../binary
+./bbr deployment --target "${BOSH_ADDRESS}" --username "${BOSH_CLIENT}" --deployment "${ERT_DEPLOYMENT_NAME}" --ca-cert "${BOSH_CA_CERT_PATH}" backup-cleanup
+cd ../bbr-pipeline-tasks-repo/tasks/bbr-backup-ert/
+
 pushd ../../../ert-backup-artifact
   ../binary/bbr deployment --target "${BOSH_ADDRESS}" \
   --username "${BOSH_CLIENT}" \
