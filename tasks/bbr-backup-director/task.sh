e4589347ff90f64f51c8bf11abf3ccc530d41ef1
Kiran Anil <anil007kiran@gmail.com>
2018-03-14 16:41:06 +0530

    Update task.sh

--- tasks/bbr-backup-director/task.sh
+++ tasks/bbr-backup-director/task.sh
@@ -7,8 +7,9 @@ bakup_time=$(date +"%F"-"%S")
 om_cmd curl -p /api/v0/deployed/director/credentials/bbr_ssh_credentials > bbr_keys.json
 BOSH_PRIVATE_KEY=$(jq -r '.credential.value.private_key_pem' bbr_keys.json)
 
-#cd ../../../binary
-#./bbr director --private-key-path <(echo "${BBR_PRIVATE_KEY}") --username bbr --host "${BOSH_ADDRESS}" backup
+cd ../../../binary
+./bbr director --private-key-path <(echo "${BBR_PRIVATE_KEY}") --username bbr --host "${BOSH_ADDRESS}" backup-cleanup
+cd ../bbr-pipeline-tasks-repo/tasks/bbr-backup-director/
 #tar -cvf director-backup.tar -- *
 pushd ../../../director-backup-artifact
   ../binary/bbr director --host "${BOSH_ADDRESS}" \
